7bd4ba94b156af22128d0c21baddd4356f906c37
Sankalp <sanklpkaushik21@gmail.com>
2025-01-15 14:30:03 +0530

    adding test cases

--- test.py
+++ test.py
@@ -12,4 +12,7 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello World!" in response.data
+    # Check that the HTML contains the rendered content from the template
+    assert b"<h1 id=\"hello\">Hello, World!</h1>" in response.data
+    assert b"<p id=\"intro\">Welcome to my interactive website. Let's make things happen!</p>" in response.data
+    assert b"<button id=\"action-btn\">Click Me!</button>" in response.data
